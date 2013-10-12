bf5af4fc0b68d9aca017f48e3222b81d8b8508e3
takahi-i <takahiko03@gmail.com>
2013-10-12 13:10:18 +0900

    Changed return type of SectionValidator from Vector into List

--- src/main/java/org/unigram/docvalidator/validator/SectionValidator.java
+++ src/main/java/org/unigram/docvalidator/validator/SectionValidator.java
@@ -1,8 +1,8 @@
 package org.unigram.docvalidator.validator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Vector;
 
 import org.unigram.docvalidator.store.FileContent;
 import org.unigram.docvalidator.store.Section;
@@ -14,10 +14,9 @@ import org.unigram.docvalidator.util.ValidationError;
  * Validate sections in documents.
  */
 public abstract class SectionValidator implements Validator {
-
   public List<ValidationError> check(FileContent file,
       ResultDistributor distributor) {
-    Vector<ValidationError> validationErrors = new Vector<ValidationError>();
+    List<ValidationError> validationErrors = new ArrayList<ValidationError>();
     for (Iterator<Section> sectionIterator =
         file.getChilds(); sectionIterator.hasNext();) {
       Section currentSection = sectionIterator.next();
@@ -26,7 +25,13 @@ public abstract class SectionValidator implements Validator {
     return validationErrors;
   }
 
-  protected abstract Vector<ValidationError> check(Section section);
+  /**
+   * To append a new Validator which use section or paragraph information, we
+   * make a new class implementing this method.
+   * @param section input section
+   * @return list of errors
+   */
+  protected abstract List<ValidationError> check(Section section);
 
   public abstract boolean loadConfiguration(Configuration conf,
       CharacterTable characterTable);
