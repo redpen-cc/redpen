337c0fd2f997af3e0a67310fb94633f679166199
takahi-i <takahiko03@gmail.com>
2013-12-11 11:57:44 +0900

    Move support methods for creating CharacterTable into CharacterTableLoader class

--- src/main/java/org/unigram/docvalidator/util/CharacterTable.java
+++ src/main/java/org/unigram/docvalidator/util/CharacterTable.java
@@ -19,25 +19,15 @@ package org.unigram.docvalidator.util;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.unigram.docvalidator.DefaultSymbols;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 /**
  * Configuration table of characters used in DocumentValidator.
@@ -51,7 +41,7 @@ public final class CharacterTable {
     } catch (FileNotFoundException e) {
       LOG.error(e.getMessage());
     }
-    loadTable(fis, characterDictionary);
+    CharacterTableLoader.loadTable(fis, characterDictionary);
   }
 
   /**
@@ -60,7 +50,7 @@ public final class CharacterTable {
    */
   public CharacterTable(InputStream stream) {
     this();
-    loadTable(stream, characterDictionary);
+    CharacterTableLoader.loadTable(stream, characterDictionary);
   }
 
   /**
@@ -102,67 +92,7 @@ public final class CharacterTable {
     }
   }
 
-  /**
-   * load input character configuration.
-   * @param stream input configuration
-   * @param characterTable TODO
-   * @return TODO
-   */
-  private boolean loadTable(InputStream stream,
-      Map<String, DVCharacter> characterTable) {
-    Document document = parseCharTableString(stream);
-    if (document == null) {
-      LOG.error("Failed to parse character table");
-      return false;
-    }
-
-    document.getDocumentElement().normalize();
-    Node root = document.getElementsByTagName("character-table").item(0);
-    NodeList nodeList = root.getChildNodes();
-    for (int temp = 0; temp < nodeList.getLength(); temp++) {
-      Node nNode = nodeList.item(temp);
-      if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-          Element element = (Element) nNode;
-          if (element.getNodeName().equals("character")) {
-            DVCharacter currentChar = createCharacter(element);
-            characterTable.put(currentChar.getName(), currentChar);
-          } else {
-            LOG.error("Invalid Node Name: " + element.getNodeName());
-            return false;
-          }
-      }
-    }
-    LOG.info("Succeeded to load character table");
-    return false;
-  }
-
-  private Document parseCharTableString(InputStream input) {
-    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-    Document doc = null;
-    try {
-      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-      doc = dBuilder.parse(input);
-    } catch (SAXException e) {
-      LOG.error(e.getMessage());
-    } catch (IOException e) {
-      LOG.error(e.getMessage());
-    } catch (ParserConfigurationException e) {
-      LOG.error(e.getMessage());
-    }
-    return doc;
-  }
-
-  private DVCharacter createCharacter(Element element) {
-    DVCharacter character = new DVCharacter(
-        element.getAttribute("name"),
-        element.getAttribute("value"),
-        element.getAttribute("invalid-chars"),
-        Boolean.parseBoolean(element.getAttribute("before-space")),
-        Boolean.parseBoolean(element.getAttribute("after-space")));
-    return character;
-  }
-
-  private static Logger LOG = LoggerFactory.getLogger(CharacterTable.class);
+  static Logger LOG = LoggerFactory.getLogger(CharacterTable.class);
 
   private Map<String, DVCharacter> characterDictionary;
 }
---
+++ src/main/java/org/unigram/docvalidator/util/CharacterTableLoader.java
@@ -0,0 +1,78 @@
+package org.unigram.docvalidator.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class CharacterTableLoader {
+  /**
+   * load input character configuration.
+   * @param stream input configuration
+   * @param characterTable TODO
+   * @return TODO
+   */
+  public final static boolean loadTable(InputStream stream,
+      Map<String, DVCharacter> characterTable) {
+    Document document = CharacterTableLoader.parseCharTableString(stream);
+    if (document == null) {
+      CharacterTable.LOG.error("Failed to parse character table");
+      return false;
+    }
+
+    document.getDocumentElement().normalize();
+    Node root = document.getElementsByTagName("character-table").item(0);
+    NodeList nodeList = root.getChildNodes();
+    for (int temp = 0; temp < nodeList.getLength(); temp++) {
+      Node nNode = nodeList.item(temp);
+      if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+          Element element = (Element) nNode;
+          if (element.getNodeName().equals("character")) {
+            DVCharacter currentChar = CharacterTableLoader.createCharacter(element);
+            characterTable.put(currentChar.getName(), currentChar);
+          } else {
+            CharacterTable.LOG.error("Invalid Node Name: " + element.getNodeName());
+            return false;
+          }
+      }
+    }
+    CharacterTable.LOG.info("Succeeded to load character table");
+    return false;
+  }
+
+  private static Document parseCharTableString(InputStream input) {
+    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    Document doc = null;
+    try {
+      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+      doc = dBuilder.parse(input);
+    } catch (SAXException e) {
+      CharacterTable.LOG.error(e.getMessage());
+    } catch (IOException e) {
+      CharacterTable.LOG.error(e.getMessage());
+    } catch (ParserConfigurationException e) {
+      CharacterTable.LOG.error(e.getMessage());
+    }
+    return doc;
+  }
+
+  private static DVCharacter createCharacter(Element element) {
+    DVCharacter character = new DVCharacter(
+        element.getAttribute("name"),
+        element.getAttribute("value"),
+        element.getAttribute("invalid-chars"),
+        Boolean.parseBoolean(element.getAttribute("before-space")),
+        Boolean.parseBoolean(element.getAttribute("after-space")));
+    return character;
+  }
+
+}
