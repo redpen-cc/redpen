9ca5d4bc681eaf520ac7915f2161157638c9b3a7
takahi-i <takahiko03@gmail.com>
2014-01-06 16:55:25 +0900

    Fix SpaceBeginingOfStenceValidator
    - Fixed the exception when input string is zero length

--- src/main/java/org/unigram/docvalidator/validator/sentence/SpaceBegginingOfSentenceValidator.java
+++ src/main/java/org/unigram/docvalidator/validator/sentence/SpaceBegginingOfSentenceValidator.java
@@ -36,7 +36,7 @@ public class SpaceBegginingOfSentenceValidator implements SentenceValidator {
   public List<ValidationError> check(Sentence sentence) {
     List<ValidationError> result = new ArrayList<ValidationError>();
     String content = sentence.content;
-    if (!sentence.isStartaragraph
+    if (!sentence.isStartaragraph && content.length() > 0
         && !String.valueOf(content.charAt(0)).equals(" ")) {
       result.add(new ValidationError(
           "Space not exist the beggining of sentence.",
