9370f9faf9e3a9b51d12cd29e6f0f8f62b6d7011
takahi-i <takahiko03@gmail.com>
2014-06-27 11:09:43 +0900

    Refactoring InvalidExpressionValidator extracting ResourceLoader class

--- redpen-core/src/main/java/org/bigram/docvalidator/validator/sentence/InvalidExpressionValidator.java
+++ redpen-core/src/main/java/org/bigram/docvalidator/validator/sentence/InvalidExpressionValidator.java
@@ -21,6 +21,8 @@ import org.bigram.docvalidator.DocumentValidatorException;
 import org.bigram.docvalidator.config.CharacterTable;
 import org.bigram.docvalidator.model.Sentence;
 import org.bigram.docvalidator.util.FileLoader;
+import org.bigram.docvalidator.util.ResourceExtractor;
+import org.bigram.docvalidator.util.ResourceLoader;
 import org.bigram.docvalidator.util.WordListExtractor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -82,18 +84,14 @@ public class InvalidExpressionValidator implements SentenceValidator {
       CharacterTable characterTable)
       throws DocumentValidatorException {
     String lang = characterTable.getLang();
-
     WordListExtractor extractor = new WordListExtractor();
-    FileLoader loader = new FileLoader(extractor);
+    ResourceLoader loader = new ResourceLoader(extractor);
 
     LOG.info("Loading default invalid expression dictionary for " +
         "\"" + lang + "\".");
     String defaultDictionaryFile = DEFAULT_RESOURCE_PATH
         + "/invalid-" + lang + ".dat";
-    InputStream inputStream = getClass()
-        .getClassLoader()
-        .getResourceAsStream(defaultDictionaryFile);
-    if (loader.loadFile(inputStream) == 0) {
+    if (loader.loadInternalResource(defaultDictionaryFile)) {
       LOG.info("Succeeded to load default dictionary.");
     } else {
       LOG.info("Failed to load default dictionary.");
@@ -104,12 +102,13 @@ public class InvalidExpressionValidator implements SentenceValidator {
       LOG.error("Dictionary file is not specified.");
     } else {
       LOG.info("user dictionary file is " + confFile);
-      if (loader.loadFile(confFile) != 0) {
+      if (loader.loadExternalFile(confFile)) {
         LOG.info("Succeeded to load specified user dictionary.");
       } else {
         LOG.error("Failed to load user dictionary.");
       }
     }
+
     invalidExpressions = extractor.get();
     return true;
   }
