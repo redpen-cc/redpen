05ed691ce181c2cb5d60d177b3142b361eb0872e
Yusuke Yamamoto <yusuke@mac.com>
2014-08-01 12:37:44 +0900

    migrate if to switch

--- redpen-core/src/main/java/org/bigram/docvalidator/validator/ValidatorFactory.java
+++ redpen-core/src/main/java/org/bigram/docvalidator/validator/ValidatorFactory.java
@@ -14,42 +14,42 @@ import org.bigram.docvalidator.validator.sentence.*;
 public class ValidatorFactory {
 
   public static Validator<?> getInstance(ValidatorConfiguration config,
-      CharacterTable characterTable)
+                                         CharacterTable characterTable)
       throws DocumentValidatorException {
-
-    if ("SentenceLength".equals(config.getConfigurationName())) {
-      return new SentenceLengthValidator(config, characterTable);
-    } else if ("InvalidExpression".equals(config.getConfigurationName())) {
-      return new InvalidExpressionValidator(config, characterTable);
-    } else if ("InvalidWord".equals(config.getConfigurationName())) {
-      return new InvalidWordValidator(config, characterTable);
-    } else if ("SpaceAfterPeriod".equals(config.getConfigurationName())) {
-      return new SpaceBeginningOfSentenceValidator(config, characterTable);
-    } else if ("CommaNumber".equals(config.getConfigurationName())) {
-      return new CommaNumberValidator(config, characterTable);
-    } else if ("WordNumber".equals(config.getConfigurationName())) {
-      return new WordNumberValidator(config, characterTable);
-    } else if ("SuggestExpression".equals(config.getConfigurationName())) {
-      return new SuggestExpressionValidator(config, characterTable);
-    } else if ("InvalidCharacter".equals(config.getConfigurationName())) {
-      return new InvalidCharacterValidator(config, characterTable);
-    } else if ("SpaceWithSymbol".equals(config.getConfigurationName())) {
-      return new SymbolWithSpaceValidator(config, characterTable);
-    } else if ("KatakanaEndHyphen".equals(config.getConfigurationName())) {
-      return new KatakanaEndHyphenValidator(config, characterTable);
-    } else if ("KatakanaSpellCheck".equals(config.getConfigurationName())) {
-      return new KatakanaSpellCheckValidator(config, characterTable);
-    } else if ("Spelling".equals(config.getConfigurationName())) {
+    switch (config.getConfigurationName()) {
+      case "SentenceLength":
+        return new SentenceLengthValidator(config, characterTable);
+      case "InvalidExpression":
+        return new InvalidExpressionValidator(config, characterTable);
+      case "InvalidWord":
+        return new InvalidWordValidator(config, characterTable);
+      case "SpaceAfterPeriod":
+        return new SpaceBeginningOfSentenceValidator(config, characterTable);
+      case "CommaNumber":
+        return new CommaNumberValidator(config, characterTable);
+      case "WordNumber":
+        return new WordNumberValidator(config, characterTable);
+      case "SuggestExpression":
+        return new SuggestExpressionValidator(config, characterTable);
+      case "InvalidCharacter":
+        return new InvalidCharacterValidator(config, characterTable);
+      case "SpaceWithSymbol":
+        return new SymbolWithSpaceValidator(config, characterTable);
+      case "KatakanaEndHyphen":
+        return new KatakanaEndHyphenValidator(config, characterTable);
+      case "KatakanaSpellCheck":
+        return new KatakanaSpellCheckValidator(config, characterTable);
+      case "Spelling":
         return new SpellingValidator(config, characterTable);
-    } if ("SectionLength".equals(config.getConfigurationName())) {
-      return new SectionLengthValidator(config, characterTable);
-    } else if ("MaxParagraphNumber".equals(config.getConfigurationName())) {
-      return new ParagraphNumberValidator(config, characterTable);
-    } else if ("ParagraphStartWith".equals(config.getConfigurationName())) {
-      return new ParagraphStartWithValidator(config, characterTable);
-    } else {
-      throw new DocumentValidatorException(
-          "There is no Validator like " + config.getConfigurationName());
+      case "SectionLength":
+        return new SectionLengthValidator(config, characterTable);
+      case "MaxParagraphNumber":
+        return new ParagraphNumberValidator(config, characterTable);
+      case "ParagraphStartWith":
+        return new ParagraphStartWithValidator(config, characterTable);
+      default:
+        throw new DocumentValidatorException(
+            "There is no Validator like " + config.getConfigurationName());
     }
   }
 }
