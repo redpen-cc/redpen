3ee0abe7906217f3894c667453ac880c7d25926c
Yusuke Yamamoto <yusuke@mac.com>
2014-09-19 15:44:21 +0900

    forcibly fallback to English resources when specifying non-Japanese locale introduce ValidationError factory method with line number

--- redpen-core/src/test/java/cc/redpen/validator/ValidationErrorMessageTest.java
+++ redpen-core/src/test/java/cc/redpen/validator/ValidationErrorMessageTest.java
@@ -11,7 +11,10 @@ class ValidationErrorMessageTest extends Validator<Sentence> {
     @Override
     public List<ValidationError> validate(Sentence sentence) {
         List<ValidationError> errors = new ArrayList<>();
-        errors.add(createValidationError(sentence, 1, 2, 3, "string"));
+        errors.add(createValidationError(sentence, 1, 2, 3, "sentence"));
+        errors.add(createValidationError("withKey", sentence, "sentence"));
+        errors.add(createValidationError(10, 1, 2, 3, "lineNumber"));
+        errors.add(createValidationError("withKey", 10, "lineNumber"));
         return errors;
     }
 }
