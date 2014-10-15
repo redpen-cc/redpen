97f2b91bdcd480e95b855488038082ebbf2ab857
takahi-i <takahiko03@gmail.com>
2014-10-15 11:21:39 +0900

    Update sample doc directories

--- redpen-cli/src/test/java/cc/redpen/MainTest.java
+++ redpen-cli/src/test/java/cc/redpen/MainTest.java
@@ -27,7 +27,7 @@ public class MainTest {
     public void testMain() throws RedPenException {
         String[] args = new String[]{
             "-c", "redpen-cli/sample/conf/redpen-conf-en.xml",
-            "redpen-cli/sample/doc/txt/en/sampledoc-en.txt"
+            "redpen-cli/sample-doc/en/sampledoc-en.txt"
         };
         
         Main.main(args);
