0b5d218f3dc502b0b5938a8a27cf514a7d368e46
Yusuke Yamamoto <yusuke@mac.com>
2014-11-11 00:53:21 +0900

    simplify DocumentCollectionTest / DocumentTest

--- redpen-core/src/test/java/cc/redpen/model/DocumentCollectionTest.java
+++ redpen-core/src/test/java/cc/redpen/model/DocumentCollectionTest.java
@@ -1,6 +1,5 @@
 package cc.redpen.model;
 
-import cc.redpen.tokenizer.JapaneseTokenizer;
 import org.junit.Test;
 
 import java.util.Optional;
@@ -33,15 +32,6 @@ public class DocumentCollectionTest {
         assertEquals("sentence1", doc.getDocument(0).getSection(0).getParagraph(0).getSentence(1).content);
         assertEquals(false, doc.getDocument(0).getSection(0).getParagraph(0).getSentence(1).isFirstSentence);
         assertEquals(1, doc.getDocument(0).getSection(0).getParagraph(0).getSentence(1).position);
-    }
-
-    @Test
-    public void testVoidCreateDocument() {
-        DocumentCollection doc = new DocumentCollection.Builder().addDocument(
-                new Document.DocumentBuilder()
-                        .setFileName("Foobar").build()).build();
-        assertEquals(1, doc.getNumberOfDocuments());
-        assertEquals(0, doc.getDocument(0).getNumberOfSections());
     }
 
     @Test
@@ -93,73 +83,4 @@ public class DocumentCollectionTest {
         assertEquals(false, doc.getDocument(1).getSection(0).getParagraph(0).getSentence(1).isFirstSentence);
         assertEquals(1, doc.getDocument(1).getSection(0).getParagraph(0).getSentence(1).position);
     }
-
-    @Test
-    public void testCreateDocumentWithList() {
-        DocumentCollection doc = new DocumentCollection.Builder().addDocument(
-                new Document.DocumentBuilder()
-                        .setFileName("Foobar")
-                        .addSection(0)
-                        .addSectionHeader("baz")
-                        .addParagraph()
-                        .addSentence("sentence0", 0)
-                        .addSentence("sentence1", 1)
-                        .addListBlock()
-                        .addListElement(0, "list0")
-                        .addListElement(0, "list1")
-                        .addListElement(1, "list2")
-                        .build()).build();
-
-        assertEquals(1, doc.getNumberOfDocuments());
-        assertEquals(1, doc.getDocument(0).getNumberOfSections());
-        assertEquals(0, doc.getDocument(0).getSection(0).getLevel());
-        assertEquals(Optional.of("Foobar"), doc.getDocument(0).getFileName());
-        assertEquals("baz", doc.getDocument(0).getSection(0).getHeaderContent(0).content);
-        assertEquals(1, doc.getDocument(0).getSection(0).getNumberOfParagraphs());
-        assertEquals("sentence0", doc.getDocument(0).getSection(0).getParagraph(0).getSentence(0).content);
-        assertEquals(true, doc.getDocument(0).getSection(0).getParagraph(0).getSentence(0).isFirstSentence);
-        assertEquals(0, doc.getDocument(0).getSection(0).getParagraph(0).getSentence(0).position);
-        assertEquals("sentence1", doc.getDocument(0).getSection(0).getParagraph(0).getSentence(1).content);
-        assertEquals(false, doc.getDocument(0).getSection(0).getParagraph(0).getSentence(1).isFirstSentence);
-        assertEquals(1, doc.getDocument(0).getSection(0).getParagraph(0).getSentence(1).position);
-        assertEquals(1, doc.getDocument(0).getSection(0).getNumberOfLists());
-        assertEquals(3, doc.getDocument(0).getSection(0).getListBlock(0).getNumberOfListElements());
-        assertEquals(0, doc.getDocument(0).getSection(0).getListBlock(0).getListElement(0).getLevel());
-        assertEquals("list0", doc.getDocument(0).getSection(0).getListBlock(0).getListElement(0).getSentence(0).content);
-        assertEquals(0, doc.getDocument(0).getSection(0).getListBlock(0).getListElement(1).getLevel());
-        assertEquals("list1", doc.getDocument(0).getSection(0).getListBlock(0).getListElement(1).getSentence(0).content);
-        assertEquals(1, doc.getDocument(0).getSection(0).getListBlock(0).getListElement(2).getLevel());
-        assertEquals("list2", doc.getDocument(0).getSection(0).getListBlock(0).getListElement(2).getSentence(0).content);
-    }
-
-    @Test
-    public void testSentenceIsTokenized() {
-        DocumentCollection doc = new DocumentCollection.Builder().addDocument(
-                new Document.DocumentBuilder()
-                        .setFileName("foobar")
-                        .addSection(0)
-                        .addSectionHeader("baz")
-                        .addParagraph()
-                        .addSentence("This is a foobar.", 0)
-                        .build()).build();
-        assertEquals(1, doc.getNumberOfDocuments());
-        assertEquals(1, doc.getDocument(0).getNumberOfSections());
-        assertEquals(4, doc.getDocument(0).getSection(0).getParagraph(0).getSentence(0).tokens.size());
-    }
-
-    @Test
-    public void testJapaneseSentenceIsTokenized() {
-        DocumentCollection doc = new DocumentCollection.Builder().addDocument(
-                new Document.DocumentBuilder(new JapaneseTokenizer())
-                        .setFileName("今日")
-                        .addSection(0)
-                        .addSectionHeader("天気")
-                        .addParagraph()
-                        .addSentence("今日は晴天だ。", 0)
-                        .build()).build();
-        assertEquals(1, doc.getNumberOfDocuments());
-        assertEquals(1, doc.getDocument(0).getNumberOfSections());
-        assertEquals(5, doc.getDocument(0).getSection(0).getParagraph(0).getSentence(0).tokens.size());
-    }
-
 }
