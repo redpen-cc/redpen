6e4e53a786d753ff49f6d5f9d763966f79c6c0be
Yusuke Yamamoto <yusuke@mac.com>
2014-11-30 01:41:06 +0900

    enable CLI tests separate main logic for the better testability

--- redpen-cli/src/test/java/cc/redpen/MainTest.java
+++ redpen-cli/src/test/java/cc/redpen/MainTest.java
@@ -17,32 +17,27 @@
  */
 package cc.redpen;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class MainTest {
 
-    @Ignore("Enable this to run basic validation test during development ")
     @Test
     public void testMain() throws RedPenException {
         String[] args = new String[]{
             "-c", "redpen-cli/sample/conf/redpen-conf-en.xml",
                 "redpen-cli/sample/sample-doc/en/sampledoc-en.txt"
         };
-        
-        Main.main(args);
+        Main.run(args);
     }
 
-    @Ignore("Enable this to show help")
     @Test
     public void testHelp() throws RedPenException {
-        Main.main(new String[] { "-h" });
+        Main.run(new String[] { "-h" });
     }
 
-    @Ignore("Enable this to show version")
     @Test
     public void testVersion() throws RedPenException {
-        Main.main(new String[] { "-v" });
+        Main.run(new String[] { "-v" });
     }
 
 }
