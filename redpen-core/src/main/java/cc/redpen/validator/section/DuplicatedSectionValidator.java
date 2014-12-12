52dff6960e630d811048036e5feefdba639c4f53
takahi-i <takahiko03@gmail.com>
2014-12-12 10:48:30 +0900

    redpen-306 Duplicate Section Validator
    - added misc methods (toString, equals, hashCode)
    - added final to DuplicatedSectionValidator class

--- redpen-core/src/main/java/cc/redpen/validator/section/DuplicatedSectionValidator.java
+++ redpen-core/src/main/java/cc/redpen/validator/section/DuplicatedSectionValidator.java
@@ -9,10 +9,13 @@ import cc.redpen.validator.Validator;
 
 import java.util.*;
 
-public class DuplicatedSectionValidator extends Validator {
+/**
+ * DuplicatedSectionValidator check if there are highly similar section pairs.
+ */
+final public class DuplicatedSectionValidator extends Validator {
 
     static final private double SIMILARITY_THRESHOLD = 0.9d;
-    private List<SectionVector> sectionVectors;
+    private List<SectionVector> sectionVectors = new ArrayList<>();
 
     class SectionVector {
         public final Sentence header;
@@ -22,10 +25,6 @@ public class DuplicatedSectionValidator extends Validator {
             this.header = header;
             this.sectionVector = vector;
         }
-    }
-
-    public DuplicatedSectionValidator() {
-        this.sectionVectors = sectionVectors = new ArrayList<>();
     }
 
     @Override
@@ -41,7 +40,7 @@ public class DuplicatedSectionValidator extends Validator {
                 addWords(sectionVector, sentence);
             }
         }
-        // apply to section header
+        // apply to sentences in section header
         for (Sentence headerSentence : section.getHeaderContents()) {
             addWords(sectionVector, headerSentence);
         }
@@ -93,5 +92,30 @@ public class DuplicatedSectionValidator extends Validator {
             Integer currentNum = sectionVector.get(surface);
             sectionVector.put(surface, ++currentNum);
         }
+    }
+
+    @Override
+    public String toString() {
+        return "DuplicatedSectionValidator{" +
+                "sectionVectors=" + sectionVectors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DuplicatedSectionValidator that = (DuplicatedSectionValidator) o;
+
+        if (sectionVectors != null ? !sectionVectors.equals(that.sectionVectors) : that.sectionVectors != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return sectionVectors != null ? sectionVectors.hashCode() : 0;
     }
 }
