d1072c5ea1836087b80fd9dd2b740d81ba7cec3b
takahi-i <takahiko03@gmail.com>
2014-12-24 16:15:37 +0900

    Simplify the output format of PlainFormatter

--- redpen-core/src/test/java/cc/redpen/formatter/PlainFormatterTest.java
+++ redpen-core/src/test/java/cc/redpen/formatter/PlainFormatterTest.java
@@ -1,7 +1,36 @@
 package cc.redpen.formatter;
 
-import static org.junit.Assert.*;
+import cc.redpen.model.Document;
+import cc.redpen.model.Sentence;
+import cc.redpen.tokenizer.WhiteSpaceTokenizer;
+import cc.redpen.validator.ValidationError;
+import cc.redpen.validator.Validator;
+import org.junit.Test;
 
-public class PlainFormatterTest {
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
+public class PlainFormatterTest extends Validator {
+    @Test
+    public void testConvertValidationError() {
+        ValidationError error = createValidationError(new Sentence("This is a sentence", 0));
+        Formatter formatter = new PlainFormatter();
+        Document document = new cc.redpen.model.Document.DocumentBuilder(new WhiteSpaceTokenizer())
+                .setFileName("foobar.md").build();
+        List<ValidationError> validationErrors = Arrays.asList(error);
+        String resultString = formatter.format(document, validationErrors);
+        assertEquals("foobar.md:0: ValidationError[PlainFormatterTest], plain test error at line: This is a sentence\n", resultString);
+    }
+
+    @Test
+    public void testConvertValidationErrorWithoutFileName() {
+        ValidationError error = createValidationError(new Sentence("This is a sentence", 0));
+        Formatter formatter = new PlainFormatter();
+        Document document = new cc.redpen.model.Document.DocumentBuilder(new WhiteSpaceTokenizer()).build();
+        List<ValidationError> validationErrors = Arrays.asList(error);
+        String resultString = formatter.format(document, validationErrors);
+        assertEquals("0: ValidationError[PlainFormatterTest], plain test error at line: This is a sentence\n", resultString);
+    }
 }
