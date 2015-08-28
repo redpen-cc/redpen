c121ecd31dbffe88b0c0f29ac5993cf02013c0c3
takahi-i <takahikoito@gmail.com>
2015-08-28 17:53:25 +0900

    Implement OkuriganaValidator

--- redpen-core/src/test/java/cc/redpen/validator/sentence/OkuriganaValidatorTest.java
+++ redpen-core/src/test/java/cc/redpen/validator/sentence/OkuriganaValidatorTest.java
@@ -1,10 +1,84 @@
+/*
+ * redpen: a text inspection tool
+ * Copyright (c) 2014-2015 Recruit Technologies Co., Ltd. and contributors
+ * (see CONTRIBUTORS.md)
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cc.redpen.validator.sentence;
 
-import static org.junit.Assert.*;
+import cc.redpen.RedPen;
+import cc.redpen.config.Configuration;
+import cc.redpen.config.ValidatorConfiguration;
+import cc.redpen.model.Document;
+import cc.redpen.parser.DocumentParser;
+import cc.redpen.parser.SentenceExtractor;
+import cc.redpen.validator.ValidationError;
+import org.junit.Test;
 
-/**
- * Created by takahi-i on 8/28/15.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
+
 public class OkuriganaValidatorTest {
+    @Test
+    public void testInvalidOkurigana() throws Exception {
+        String sampleText = "このタスクに長い年月を費してきた。";
+        Configuration config = new Configuration.ConfigurationBuilder()
+                .addValidatorConfig(
+                        new ValidatorConfiguration("Okurigana"))
+                .setLanguage("ja")
+                .build();
+
+        DocumentParser parser = DocumentParser.MARKDOWN;
+        List<Document> documents = new ArrayList<>();
+        Document document  = parser.parse(sampleText,
+                new SentenceExtractor(config.getSymbolTable()),
+                config.getTokenizer());
+        documents.add(document);
+
+        RedPen redPen = new RedPen(config);
+        Map<Document, List<ValidationError>> errors = redPen.validate(documents);
+
+        assertEquals(1, errors.get(documents.get(0)).size());
+        assertEquals(1, errors.get(documents.get(0)).get(0).getLineNumber());
+        assertEquals("Okurigana", errors.get(documents.get(0)).get(0).getValidatorName());
+    }
+
+    @Test
+    public void testNoInvalidOkurigana() throws Exception {
+        String sampleText = "このタスクに長い年月を費やしてきた。";
+        Configuration config = new Configuration.ConfigurationBuilder()
+                .addValidatorConfig(
+                        new ValidatorConfiguration("Okurigana"))
+                .setLanguage("ja")
+                .build();
+
+        DocumentParser parser = DocumentParser.MARKDOWN;
+        List<Document> documents = new ArrayList<>();
+        Document document  = parser.parse(sampleText,
+                new SentenceExtractor(config.getSymbolTable()),
+                config.getTokenizer());
+        documents.add(document);
+
+        RedPen redPen = new RedPen(config);
+        Map<Document, List<ValidationError>> errors = redPen.validate(documents);
+
+        assertEquals(0, errors.get(documents.get(0)).size());
+        assertEquals(0, errors.get(documents.get(0)).get(0).getLineNumber());
+    }
+
 
 }
