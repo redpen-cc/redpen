60311f35fd9c77363c08e52c2fe8b2321faf7367
takahi-i <takahikoito@gmail.com>
2015-10-07 11:00:13 +0900

    Bump version to 1.4

--- redpen-core/src/main/java/cc/redpen/RedPen.java
+++ redpen-core/src/main/java/cc/redpen/RedPen.java
@@ -46,7 +46,7 @@ import java.util.Map;
 public class RedPen {
     private static final Logger LOG = LoggerFactory.getLogger(RedPen.class);
 
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.4.0";
 
     private final Configuration configuration;
     private final SentenceExtractor sentenceExtractor;
--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.3
+VERSION=1.4
 
 run_test() {
     echo "Building RedPen Version $VERSION"
