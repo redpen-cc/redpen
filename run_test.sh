5a762724c0b4b3593156da0f6f81b154c3947c4d
Anton Keks <anton@codeborne.com>
2016-01-14 13:08:48 +0200

    run redpen-server UI tests against a running server

--- run_test.sh
+++ run_test.sh
@@ -9,18 +9,16 @@ run_test() {
     tar zxvf redpen-cli-*-assembled.tar.gz;
     cd redpen-cli-*
     bin/redpen -c conf/redpen-conf-en.xml -l 100 sample-doc/en/sampledoc-en.txt
-    if [ $? -ne 0 ]; then
-        echo "Error running application..."
-        exit 1
-    fi
     cd ../../..
 
     echo "Running sample server"
-    java -jar redpen-server/target/redpen-server.war &
+    cd redpen-server
+    java -jar target/redpen-server.war &
     if [ -z "$(pgrep redpen)" ]
     then
         echo "RedPen server is nunning as expected ..."
-        sleep 5
+        mvn test
+
         echo "Killing sample server"
         pgrep -f redpen | xargs kill
     else
