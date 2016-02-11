a96126ef1f1ce3618e18b98184ee439a0ddc893f
Dmitri Ess, Anton Keks <dmitri.ess@codeborne.com, anton@codeborne.com>
2016-02-11 15:35:56 +0200

    add deprectated ConfigurationLoader to the old location for backwards-compatibility

---
+++ redpen-core/src/main/java/cc/redpen/ConfigurationLoader.java
@@ -0,0 +1,8 @@
+package cc.redpen;
+
+/**
+ * @deprecated use {@link cc.redpen.config.ConfigurationLoader} instead
+ */
+@Deprecated
+public class ConfigurationLoader extends cc.redpen.config.ConfigurationLoader {
+}
