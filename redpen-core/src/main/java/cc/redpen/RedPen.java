ba8f289001b7966617c55244f80d3aa47e7418ff
takahi-i <takahikoito@gmail.com>
2016-02-19 12:15:52 +0900

    Bump v1.4.6

--- redpen-core/src/main/java/cc/redpen/RedPen.java
+++ redpen-core/src/main/java/cc/redpen/RedPen.java
@@ -39,7 +39,7 @@ import java.util.*;
 public class RedPen {
     private static final Logger LOG = LoggerFactory.getLogger(RedPen.class);
 
-    public static final String VERSION = "1.4.5";
+    public static final String VERSION = "1.4.6";
 
     private final Configuration configuration;
     private final SentenceExtractor sentenceExtractor;
