bf786a2c707ececeeb93bb7fd7b54369fa404a1e
Yusuke Yamamoto <yusuke@mac.com>
2016-04-08 23:46:34 +0900

    fit code formats

--- redpen-server/src/test/java/cc/redpen/server/api/CORSFilterTest.java
+++ redpen-server/src/test/java/cc/redpen/server/api/CORSFilterTest.java
@@ -9,33 +9,33 @@ import javax.servlet.http.HttpServletResponse;
 import static org.mockito.Mockito.*;
 
 public class CORSFilterTest {
-  CORSFilter filter = new CORSFilter();
-  HttpServletRequest request = mock(HttpServletRequest.class);
-  HttpServletResponse response = mock(HttpServletResponse.class);
-  FilterChain chain = mock(FilterChain.class);
+    CORSFilter filter = new CORSFilter();
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    FilterChain chain = mock(FilterChain.class);
 
-  @Test
-  public void doNothingForRegularRequests() throws Exception {
-    filter.doFilter(request, response, chain);
-    verify(response, never()).addHeader(anyString(), anyString());
-    verify(chain).doFilter(request, response);
-  }
+    @Test
+    public void doNothingForRegularRequests() throws Exception {
+        filter.doFilter(request, response, chain);
+        verify(response, never()).addHeader(anyString(), anyString());
+        verify(chain).doFilter(request, response);
+    }
 
-  @Test
-  public void addAllowOriginForCORSRequests() throws Exception {
-    when(request.getMethod()).thenReturn("GET");
-    when(request.getHeader("Origin")).thenReturn("http://blah.com");
-    filter.doFilter(request, response, chain);
-    verify(response).addHeader("Access-Control-Allow-Origin", "http://blah.com");
-    verify(chain).doFilter(request, response);
-  }
+    @Test
+    public void addAllowOriginForCORSRequests() throws Exception {
+        when(request.getMethod()).thenReturn("GET");
+        when(request.getHeader("Origin")).thenReturn("http://blah.com");
+        filter.doFilter(request, response, chain);
+        verify(response).addHeader("Access-Control-Allow-Origin", "http://blah.com");
+        verify(chain).doFilter(request, response);
+    }
 
-  @Test
-  public void doNotGenerateResponseForOPTIONSRequests() throws Exception {
-    when(request.getMethod()).thenReturn("OPTIONS");
-    when(request.getHeader("Origin")).thenReturn("http://blah.com");
-    filter.doFilter(request, response, chain);
-    verify(response).addHeader("Access-Control-Allow-Origin", "http://blah.com");
-    verify(chain, never()).doFilter(request, response);
-  }
+    @Test
+    public void doNotGenerateResponseForOPTIONSRequests() throws Exception {
+        when(request.getMethod()).thenReturn("OPTIONS");
+        when(request.getHeader("Origin")).thenReturn("http://blah.com");
+        filter.doFilter(request, response, chain);
+        verify(response).addHeader("Access-Control-Allow-Origin", "http://blah.com");
+        verify(chain, never()).doFilter(request, response);
+    }
 }
--- redpen-server/src/test/java/ui/RedPenDemoTest.java
+++ redpen-server/src/test/java/ui/RedPenDemoTest.java
@@ -15,81 +15,79 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeNoException;
 
 public class RedPenDemoTest {
-  private String redpenServerUrl = "http://localhost:8080/";
+    private String redpenServerUrl = "http://localhost:8080/";
 
-  @Before
-  public void setUp() throws Exception {
-    try {
-      new URL(redpenServerUrl).openConnection().connect();
-      // Run tests in PhantomJS by default if browser property is not set
-      if (System.getProperty("browser") == null)
-        System.setProperty("browser", "phantomjs");
-      open(redpenServerUrl);
+    @Before
+    public void setUp() throws Exception {
+        try {
+            new URL(redpenServerUrl).openConnection().connect();
+            // Run tests in PhantomJS by default if browser property is not set
+            if (System.getProperty("browser") == null)
+                System.setProperty("browser", "phantomjs");
+            open(redpenServerUrl);
+        } catch (ConnectException e) {
+            assumeNoException("RedPen server is not running, skipping UI tests", e);
+        } catch (IllegalStateException e) {
+            assumeNoException("Please install " + System.getProperty("browser") + " for UI tests to run", e);
+        }
     }
-    catch (ConnectException e) {
-      assumeNoException("RedPen server is not running, skipping UI tests", e);
+
+    @Test
+    public void redpenEditorIsPrepopulated() throws Exception {
+        String value = $("#redpen-editor").shouldBe(visible).val();
+        assertTrue(value.startsWith("Some software tools work"));
     }
-    catch (IllegalStateException e) {
-      assumeNoException("Please install " + System.getProperty("browser") + " for UI tests to run", e);
+
+    @Test
+    public void userCanChooseSampleTexts() throws Exception {
+        if ($(".navbar-toggle").isDisplayed())
+            $(".navbar-toggle").click();
+
+        $("#themes").click();
+        $(By.linkText("JAPANESE TEXT")).click();
+
+        String value = $("#redpen-editor").shouldBe(visible).val();
+        assertTrue(value.startsWith("最近利用"));
     }
-  }
 
-  @Test
-  public void redpenEditorIsPrepopulated() throws Exception {
-    String value = $("#redpen-editor").shouldBe(visible).val();
-    assertTrue(value.startsWith("Some software tools work"));
-  }
+    @Test
+    public void userCanClearTheText() throws Exception {
+        $("[title='Clear text']").click();
+        $("#redpen-editor").shouldBe(empty);
+    }
 
-  @Test
-  public void userCanChooseSampleTexts() throws Exception {
-    if ($(".navbar-toggle").isDisplayed())
-      $(".navbar-toggle").click();
+    @Test
+    public void textIsValidatedAsItEntered() throws Exception {
+        $("#redpen-editor").val("Hello Wodrl");
+        $("#redpen-errors").shouldHave(text("RedPen found 1 error"));
 
-    $("#themes").click();
-    $(By.linkText("JAPANESE TEXT")).click();
+        ElementsCollection errors = $$(".redpen-error-list .redpen-error-message").shouldHaveSize(1);
+        errors.get(0).shouldHave(text("Found possibly misspelled word \"Wodrl\"."));
+        errors.get(0).find(".redpen-error-validator").shouldHave(text("Spelling"));
+    }
 
-    String value = $("#redpen-editor").shouldBe(visible).val();
-    assertTrue(value.startsWith("最近利用"));
-  }
+    @Test
+    public void validatorsCanBeDisabled() throws Exception {
+        $("input[type=checkbox][value=Spelling]").click();
+        $("#redpen-editor").val("Hello Wodrl");
+        $("#redpen-errors").shouldHave(text("RedPen found 0 errors"));
+    }
 
-  @Test
-  public void userCanClearTheText() throws Exception {
-    $("[title='Clear text']").click();
-    $("#redpen-editor").shouldBe(empty);
-  }
+    @Test
+    public void validatorPropertiesCanBeChanged() throws Exception {
+        SelenideElement validatorProperties = $(".redpen-validator-properties[name=SentenceLength]");
+        validatorProperties.click();
 
-  @Test
-  public void textIsValidatedAsItEntered() throws Exception {
-    $("#redpen-editor").val("Hello Wodrl");
-    $("#redpen-errors").shouldHave(text("RedPen found 1 error"));
+        $(".popover-title").should(appear).shouldHave(text("SentenceLength properties"));
+        $(".popover-content input[type=text]").shouldHave(value("max_len=120")).val("max_len=10");
+        $(".popover-content button[type=submit]").click();
 
-    ElementsCollection errors = $$(".redpen-error-list .redpen-error-message").shouldHaveSize(1);
-    errors.get(0).shouldHave(text("Found possibly misspelled word \"Wodrl\"."));
-    errors.get(0).find(".redpen-error-validator").shouldHave(text("Spelling"));
-  }
+        validatorProperties.shouldHave(text("max_len=10"));
 
-  @Test
-  public void validatorsCanBeDisabled() throws Exception {
-    $("input[type=checkbox][value=Spelling]").click();
-    $("#redpen-editor").val("Hello Wodrl");
-    $("#redpen-errors").shouldHave(text("RedPen found 0 errors"));
-  }
+        $("#redpen-editor").val("This is a very long sentence of over ten words.");
+        $("#redpen-errors").shouldHave(text("RedPen found 2 errors"));
 
-  @Test
-  public void validatorPropertiesCanBeChanged() throws Exception {
-    SelenideElement validatorProperties = $(".redpen-validator-properties[name=SentenceLength]");
-    validatorProperties.click();
-
-    $(".popover-title").should(appear).shouldHave(text("SentenceLength properties"));
-    $(".popover-content input[type=text]").shouldHave(value("max_len=120")).val("max_len=10");
-    $(".popover-content button[type=submit]").click();
-
-    validatorProperties.shouldHave(text("max_len=10"));
-
-    $("#redpen-editor").val("This is a very long sentence of over ten words.");
-    $("#redpen-errors").shouldHave(text("RedPen found 2 errors"));
-
-    $$(".redpen-error-message").get(0).shouldHave(text("The length of the sentence (47) exceeds the maximum of 10."), text("SentenceLength"));
-    $$(".redpen-error-message").get(1).shouldHave(text("\"very\" is considered a weak expression."), text("WeakExpression"));
-  }
+        $$(".redpen-error-message").get(0).shouldHave(text("The length of the sentence (47) exceeds the maximum of 10."), text("SentenceLength"));
+        $$(".redpen-error-message").get(1).shouldHave(text("\"very\" is considered a weak expression."), text("WeakExpression"));
+    }
 }
