306560b218b42d049c18240c49beb4bda7359434
takahi-i <takahikoito@gmail.com>
2016-04-20 14:48:37 +0900

    Add tests on list blocks

--- redpen-core/src/test/java/cc/redpen/parser/review/ReVIEWParserTest.java
+++ redpen-core/src/test/java/cc/redpen/parser/review/ReVIEWParserTest.java
@@ -59,6 +59,69 @@ public class ReVIEWParserTest {
         assertEquals("About Gekioko.", doc.getSection(0).getParagraph(0).getSentence(0).getContent());
     }
 
+    @Test
+    public void testGenerateDocumentWithList() {
+        String sampleText = "There are several railway companies in Japan as follows.\n";
+        sampleText += "\n";
+        sampleText += "* Tokyu\n";
+        sampleText += "** Toyoko Line\n";
+        sampleText += "** Denentoshi Line\n";
+        sampleText += "* Keio\n";
+        sampleText += "* Odakyu\n";
+
+        Document doc = createFileContent(sampleText);
+        assertEquals(5, doc.getSection(0).getListBlock(0).getNumberOfListElements());
+        assertEquals("Tokyu", doc.getSection(0).getListBlock(0).getListElement(0).getSentence(0).getContent());
+        assertEquals(1, doc.getSection(0).getListBlock(0).getListElement(0).getLevel());
+        assertEquals(3, doc.getSection(0).getListBlock(0).getListElement(0).getSentence(0).getLineNumber());
+        assertEquals(2, doc.getSection(0).getListBlock(0).getListElement(0).getSentence(0).getStartPositionOffset());
+
+        assertEquals("Toyoko Line", doc.getSection(0).getListBlock(0).getListElement(1).getSentence(0).getContent());
+        assertEquals(2, doc.getSection(0).getListBlock(0).getListElement(1).getLevel());
+        assertEquals(4, doc.getSection(0).getListBlock(0).getListElement(1).getSentence(0).getLineNumber());
+        assertEquals(3, doc.getSection(0).getListBlock(0).getListElement(1).getSentence(0).getStartPositionOffset());
+
+        assertEquals("Denentoshi Line", doc.getSection(0).getListBlock(0).getListElement(2).getSentence(0).getContent());
+        assertEquals(2, doc.getSection(0).getListBlock(0).getListElement(2).getLevel());
+        assertEquals(5, doc.getSection(0).getListBlock(0).getListElement(2).getSentence(0).getLineNumber());
+        assertEquals(3, doc.getSection(0).getListBlock(0).getListElement(2).getSentence(0).getStartPositionOffset());
+
+        assertEquals("Keio", doc.getSection(0).getListBlock(0).getListElement(3).getSentence(0).getContent());
+        assertEquals(1, doc.getSection(0).getListBlock(0).getListElement(3).getLevel());
+        assertEquals(6, doc.getSection(0).getListBlock(0).getListElement(3).getSentence(0).getLineNumber());
+        assertEquals(2, doc.getSection(0).getListBlock(0).getListElement(3).getSentence(0).getStartPositionOffset());
+
+        assertEquals("Odakyu", doc.getSection(0).getListBlock(0).getListElement(4).getSentence(0).getContent());
+        assertEquals(1, doc.getSection(0).getListBlock(0).getListElement(4).getLevel());
+        assertEquals(7, doc.getSection(0).getListBlock(0).getListElement(4).getSentence(0).getLineNumber());
+        assertEquals(2, doc.getSection(0).getListBlock(0).getListElement(4).getSentence(0).getStartPositionOffset());
+    }
+
+    @Test
+    public void testLabelledList() {
+        String sampleText = "= SampleDoc\n" +
+                "v0.0.2, 2015-11-17\n" +
+                ":last-update-label!:\n" +
+                "\n" +
+                "== 用語定義\n" +
+                "ユビキタス言語を定義します。\n" +
+                "\n" +
+                ": Some word\n" +
+                "なにかの意味をのせて用例をのせます。\n" +
+                "\n" +
+                ": リリース\n" +
+                "ソフトウェアを顧客に提供することです。\n" +
+                "\n" +
+                ": redpen\n" +
+                "RedPen はオープンソースの校正ツールです。RedPen は技術文書が文書規約に従って書かれているかを自動検査します。 現在の RedPen 日本語ドキュメントは十分検査されておりません。校正にはもう少々時間がかかる予定です。誤りなど見つかりましたら、https://github.com/redpen-cc/redpen-doc-ja に Issue 登録しておしらせ頂けると幸いです。";
+
+        Document doc = createFileContent(sampleText);
+        assertEquals(3, doc.getSection(1).getListBlock(0).getNumberOfListElements());
+        assertEquals("なにかの意味をのせて用例をのせます。", doc.getSection(1).getListBlock(0).getListElement(0).getSentence(0).getContent());
+        assertEquals(15, doc.getSection(1).getListBlock(0).getListElement(2).getSentence(0).getLineNumber());
+
+    }
+
     private Document createFileContent(String inputDocumentString) {
         DocumentParser parser = DocumentParser.REVIEW;
         Document doc = null;
