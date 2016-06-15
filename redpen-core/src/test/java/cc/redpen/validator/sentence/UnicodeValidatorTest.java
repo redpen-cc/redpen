5c777d168fd9009f6feaf7022d204b04d027cf8e
Takahiro Yoshimura <altakey@gmail.com>
2016-06-15 11:46:05 +0900

    Ported Unicode Normalization Form D Validator

---
+++ redpen-core/src/test/java/cc/redpen/validator/sentence/UnicodeValidatorTest.java
@@ -0,0 +1,73 @@
+/**
+ * redpen: a text inspection tool
+ * Copyright (c) 2014-2015 Recruit Technologies Co., Ltd. and contributors
+ * (see CONTRIBUTORS.md)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cc.redpen.validator.sentence;
+
+import cc.redpen.RedPen;
+import cc.redpen.RedPenException;
+import cc.redpen.config.Configuration;
+import cc.redpen.config.ValidatorConfiguration;
+import cc.redpen.model.Document;
+import cc.redpen.model.Sentence;
+import cc.redpen.parser.DocumentParser;
+import cc.redpen.parser.SentenceExtractor;
+import cc.redpen.tokenizer.JapaneseTokenizer;
+import cc.redpen.validator.ValidationError;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class UnicodeValidatorTest {
+    @Test
+    public void testInvalid() throws RedPenException {
+        String sampleText = "フォルダを開きます。";
+        Configuration config = Configuration.builder("ja")
+                .addValidatorConfig(new ValidatorConfiguration("Unicode"))
+                .build();
+        DocumentParser parser = DocumentParser.MARKDOWN;
+        List<Document> documents = new ArrayList<>();
+        Document document  = parser.parse(sampleText, new SentenceExtractor(config.getSymbolTable()),
+                config.getTokenizer());
+        documents.add(document);
+
+        RedPen redPen = new RedPen(config);
+        Map<Document, List<ValidationError>> errors = redPen.validate(documents);
+
+        Assert.assertEquals(1, errors.get(documents.get(0)).size());
+    }
+
+    @Test
+    public void testValid() throws RedPenException {
+        String sampleText = "フォルダを開きます。";
+        Configuration config = Configuration.builder("ja")
+                .addValidatorConfig(new ValidatorConfiguration("Unicode"))
+                .build();
+        DocumentParser parser = DocumentParser.MARKDOWN;
+        List<Document> documents = new ArrayList<>();
+        Document document  = parser.parse(sampleText, new SentenceExtractor(config.getSymbolTable()),
+                config.getTokenizer());
+        documents.add(document);
+
+        RedPen redPen = new RedPen(config);
+        Map<Document, List<ValidationError>> errors = redPen.validate(documents);
+
+        Assert.assertEquals(0, errors.get(documents.get(0)).size());
+    }
+}
