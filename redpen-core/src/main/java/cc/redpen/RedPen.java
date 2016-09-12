00f7023ed4575e9da3e293f83527fe107cd50f40
takahi-i <takahikoito@gmail.com>
2016-09-12 13:08:31 +0900

    Bump RedPen version

--- redpen-core/src/main/java/cc/redpen/RedPen.java
+++ redpen-core/src/main/java/cc/redpen/RedPen.java
@@ -40,7 +40,7 @@ import java.util.*;
 public class RedPen {
     private static final Logger LOG = LoggerFactory.getLogger(RedPen.class);
 
-    public static final String VERSION = "1.7.1";
+    public static final String VERSION = "1.7.2";
 
     private final Configuration configuration;
     private final SentenceExtractor sentenceExtractor;
